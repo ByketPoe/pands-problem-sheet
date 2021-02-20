b036dadbc445750319ac392d4dac0732664a79db
ByketPoe <32248836+ByketPoe@users.noreply.github.com>
2021-02-20 17:24:42 +0000

    initial commit of secondstring. not working yet, just laying out the code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pands-problem-sheet
 Submission for PandS problem sheets
+
+Week 2 bmi.py 
+    References:
+        Round function: https://www.w3schools.com/python/ref_func_round.asp
+        
+
+Week 3 secondstring.py
+    References:
+        
---
+++ secondstring.py
@@ -0,0 +1,19 @@
+# secondstring.py
+# The purpose of this program is to take in text and return every second letter in reverse order.
+# author: Emma Farrell
+
+# The user is asked for some text. 
+# The input function is used to request this text and take it in.
+# The text is passed to the variable inputText.
+# Technically, according to the Whirlwind Tour of Python book, inputText is a pointer pointing to the value, as opposed to inputText storing the value.
+# But let's not get too carried away.
+inputText = input("Please enter your sentence: ")
+
+# Need to reverse the text
+
+
+# Need to remove every other character.
+
+
+# Output result
+print()
