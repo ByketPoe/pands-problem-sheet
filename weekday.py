8c5733930a74d380fd640c64985d3c2e648d2293
ByketPoe <32248836+ByketPoe@users.noreply.github.com>
2021-02-24 23:38:49 +0000

    initial attempt at week5

---
+++ weekday.py
@@ -0,0 +1,23 @@
+# weekday.py
+# The purpose of this program is to 
+# author: Emma Farrell
+
+# Write a program that outputs whether or not today is a weekday.
+
+# (You will need to search the web to find how you work out what day it is)
+# USE DATETIME MODULE
+
+# An example of running this program on a Thursday is given below.
+
+# $ python weekday.py
+# Yes, unfortunately today is a weekday.
+
+
+# An example of running it on a Saturday is as follows:
+
+# $ python weekday.py
+# It is the weekend, yay!
+
+import datetime
+
+print(datetime.datetime.now())
